74178aef1b6c61977b0fcf91a50f15df8159cf6c
MLuqman150 <muhammadluqman12080@outlook.com>
2024-11-28 23:44:23 +0500

    Highlight active page in navbar with color and underline

--- Frontend/renderer.js
+++ Frontend/renderer.js
@@ -49,22 +49,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const useTransparentHuman = document.getElementById('useTransparentHuman');
     const navLinks = document.querySelectorAll(".nav-link a")
 
-    if (navLinks) {
-        for (let i = 0; i < navLinks.length; i++) {
-            navLinks[i].addEventListener("click", function (event) {
-                // Loop through all links
-                for (let j = 0; j < navLinks.length; j++) {
-                    // Remove 'active' class only from links that are not the clicked one
-                    if (navLinks[j] !== event.target) {
-                        navLinks[j].id = "";
-                    }
-                }
+    // if (navLinks) {
+    //     for (let i = 0; i < navLinks.length; i++) {
+    //         navLinks[i].addEventListener("click", function (event) {
+    //             // Loop through all links
+    //             for (let j = 0; j < navLinks.length; j++) {
+    //                 // Remove 'active' class only from links that are not the clicked one
+    //                 if (navLinks[j] !== event.target) {
+    //                     navLinks[j].id = "";
+    //                 }
+    //             }
 
-                // Add 'active' class to the clicked link
-                event.target.id = "active";
-            });
-        }
-    }
+    //             // Add 'active' class to the clicked link
+    //             event.target.id = "active";
+    //         });
+    //     }
+    // }
 
     if ((uploadArea && imageUpload) || (folderUpload && folderUploadArea)) {
         // Handle image upload area click for single image
